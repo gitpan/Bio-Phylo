96eeae99aa3ee5a2a62c3eaa5c17e1dd9f51c90c
Rutger Vos <rutgeraldo@gmail.com>
2010-10-22 10:19:21 +0000

    Import of RVOSA/Bio-Phylo-0.24 from CPAN.
    gitpan-cpan-distribution: Bio-Phylo
    gitpan-cpan-version:      0.24
    gitpan-cpan-path:         RVOSA/Bio-Phylo-0.24.tar.gz
    gitpan-cpan-author:       RVOSA
    gitpan-cpan-maturity:     released

--- lib/Bio/Phylo.pm
+++ lib/Bio/Phylo.pm
@@ -1,4 +1,4 @@
-# $Id: Phylo.pm 1391 2010-10-22 10:06:41Z rvos $
+# $Id: Phylo.pm 1392 2010-10-22 10:17:42Z rvos $
 package Bio::Phylo;
 use strict;
 
@@ -31,9 +31,9 @@ BEGIN {
 require Bio::Phylo::Mediators::TaxaMediator;  
 
 # Include the revision number from subversion in $VERSION
-my $rev = '$Id: Phylo.pm 1391 2010-10-22 10:06:41Z rvos $';
+my $rev = '$Id: Phylo.pm 1392 2010-10-22 10:17:42Z rvos $';
 $rev =~ s/^[^\d]+(\d+)\b.*$/$1/;
-$VERSION = "0.23";
+$VERSION = "0.24";
 $VERSION .= "_$rev";
 {
     my $taxamediator = 'Bio::Phylo::Mediators::TaxaMediator';
@@ -1072,7 +1072,7 @@ Also see the manual: L<Bio::Phylo::Manual> and L<http://rutgervos.blogspot.com>
 
 =head1 REVISION
 
- $Id: Phylo.pm 1391 2010-10-22 10:06:41Z rvos $
+ $Id: Phylo.pm 1392 2010-10-22 10:17:42Z rvos $
 
 =cut
 
